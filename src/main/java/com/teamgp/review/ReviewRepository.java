15d83f4e2874d91214f71607496d6c9eb540c075
gpborla <gpborla+github@gmail.com>
2018-08-30 17:27:33 +0800

    Configured review deletion authorization. Only reviewer and Admin can delete reviews.

--- src/main/java/com/teamgp/review/ReviewRepository.java
+++ src/main/java/com/teamgp/review/ReviewRepository.java
@@ -2,6 +2,15 @@ package com.teamgp.review;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface ReviewRepository extends PagingAndSortingRepository<Review, Long> {
+  @Override
+  @PreAuthorize("hasRole('ROLE_ADMIN') or @ReviewRepository.findOne(#id)?.reviewer?.userName == authentication.name")
+  void deleteById(@Param("id") Long id);
+
+  @Override
+  @PreAuthorize("hasRole('ROLE_ADMIN') or #review.reviewer?.userName == authentication.name")
+  void delete(@Param("review") Review entity);
 }
